586d8fdd713d465e187bda6ef78e9079b81a10d1
toseefkhan403 <toseefkhan403@gmail.com>
2024-05-04 01:43:57 +0530

    add home page design with menu cards

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,9 +1,7 @@
-import 'package:daily_wrapped/providers/spotify_auth_notifier.dart';
+import 'package:daily_wrapped/views/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_carousel/flutter_custom_carousel.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:provider/provider.dart';
-import 'package:spotify/spotify.dart' hide Offset;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,83 +11,200 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<TrackSimple> playHistory = [];
-  int _selectedIndex = 0;
-  late CustomCarouselScrollController _controller;
-
-  @override
-  void initState() {
-    context.read<SpotifyAuthNotifier>().recentlyPlayed();
-    _controller = CustomCarouselScrollController();
-    super.initState();
-  }
+  int? _activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    playHistory = context.watch<SpotifyAuthNotifier>().playHistory;
-    final imgUrl = playHistory[_selectedIndex].artists?[0].images?[0].url ?? "https://upload.wikimedia.org/wikipedia/en/7/74/Ye_album_cover.jpg";
-
     return Scaffold(
-      backgroundColor: const Color(0xFF1F1B2E),
-      body: SafeArea(
-        bottom: false,
-        minimum: const EdgeInsets.only(top: 32),
+      backgroundColor: AppColors.bgBlack,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: _buildCarousel(),
       ),
     );
   }
 
   Widget _buildCarousel() {
-    List<Widget> items = List.generate(15, (i) => _Card(i));
+    List<Widget> items = [];
+    for (int i = 0; i < menuItems.length; i++) {
+      items.add(MenuCard(
+        example: menuItems[i],
+        selected: i == _activeIndex,
+        key: ValueKey(i),
+      ));
+    }
 
-    // See `card_deck_view.dart` for a fully commented example of using
-    // Animate with CustomCarousel.
     return CustomCarousel(
-      itemCountBefore: 4,
-      itemCountAfter: 4,
+      alignment: Alignment.center,
+      itemCountBefore: 2,
+      itemCountAfter: 2,
+      scrollSpeed: 1.5,
       loop: true,
-      tapToSelect: true,
-      scrollSpeed: 5,
       effectsBuilder: CustomCarousel.effectsBuilderFromAnimate(
         effects: EffectList()
-            .tint(begin: 0.5, color: const Color(0xFF1F1B2E))
-            .flipV(begin: -0.15, end: 0.5)
-            .slideY(end: 0.5)
-            .scaleXY(begin: 0.75, curve: Curves.fastEaseInToSlowEaseOut)
-            .align(
-          begin: const Alignment(0, -1),
-          end: const Alignment(0, 1),
-          curve: Curves.easeIn,
-        ),
+            .shimmer(
+              delay: 60.ms,
+              duration: 140.ms,
+              color: Colors.white24,
+              angle: 0.3,
+            )
+            .blurXY(delay: 0.ms, duration: 100.ms, begin: 4)
+            .blurXY(delay: 100.ms, end: 4)
+            .slideY(delay: 0.ms, duration: 200.ms, begin: -3.25, end: 3.25)
+            .flipH(begin: -0.2, end: 0.2),
       ),
+      onSettledItemChanged: (i) => setState(() => _activeIndex = i),
       children: items,
     );
   }
 }
 
-class _Card extends StatelessWidget {
-  const _Card(this.index, {Key? key}) : super(key: key);
-  final int index;
+class MenuCard extends StatelessWidget {
+  const MenuCard({
+    super.key,
+    required this.example,
+    this.selected = false,
+  });
+
+  final MenuItemData example;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.7,
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage('https://qodeinteractive.com/magazine/wp-content/uploads/2020/06/16-Tame-Impala.jpg'),
-              fit: BoxFit.cover,
-            ),
-            border: const Border(
-              top: BorderSide(color: Colors.white38, width: 2),
-            ),
-            borderRadius: BorderRadius.circular(12),
+    Widget card = AspectRatio(
+      aspectRatio: 0.9,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: example.color,
+          image: DecorationImage(
+            image: AssetImage('assets/images/${example.img}.png'),
+            fit: BoxFit.cover,
           ),
+          borderRadius: BorderRadius.circular(16),
+          border: const Border(
+            top: BorderSide(color: Colors.white30, width: 2),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 2.0),
+              child: Text(
+                example.title,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'Franie',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            if (selected)
+              Align(
+                alignment: Alignment.bottomRight,
+                child: _buildTryBtn(),
+              )
+            else
+              const SizedBox(
+                height: 33,
+              ),
+          ],
         ),
       ),
     );
+
+    if (selected) {
+      card = MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () => _goto(context, example),
+          child: card,
+        ),
+      );
+    }
+
+    return card;
+  }
+
+  void _goto(BuildContext context, MenuItemData example) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: example.builder),
+    );
+  }
+
+  Widget _buildTryBtn() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(999),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 8,
+            offset: Offset(2, 4),
+          ),
+        ],
+      ),
+      child: const Text(
+        'Generate with AI ✨',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+          color: Colors.black,
+        ),
+      ),
+    ).animate().fadeIn(delay: 100.ms).moveX(begin: -6, curve: Curves.easeOut);
   }
 }
+
+class MenuItemData {
+  const MenuItemData({
+    required this.img,
+    required this.title,
+    required this.color,
+    required this.builder,
+  });
+
+  final String img;
+  final String title;
+  final Color color;
+  final WidgetBuilder builder;
+}
+
+final List<MenuItemData> menuItems = [
+  MenuItemData(
+    img: 'weekend-removebg-preview',
+    title: 'Recently Played',
+    color: AppColors.maroon,
+    builder: (_) => const HomePage(),
+  ),
+  MenuItemData(
+    img: 'taylor',
+    title: 'Top Artists',
+    color: AppColors.skyBlue,
+    builder: (_) => const HomePage(),
+  ),
+  MenuItemData(
+    img: 'dua',
+    title: 'Top Songs',
+    color: AppColors.pinkishWhite,
+    builder: (_) => const HomePage(),
+  ),
+  MenuItemData(
+    img: 'eminem',
+    title: 'Your music personality',
+    color: AppColors.pinkishWhite,
+    builder: (_) => const HomePage(),
+  ),
+  MenuItemData(
+    img: 'harry',
+    title: 'Your mood based on your recent plays',
+    color: AppColors.pink,
+    builder: (_) => const HomePage(),
+  ),
+];
---
+++ lib/views/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff6a00ba);
+  static const accentColor = Color(0xFFCFF469);
+  static const midnightPurple = Color(0xFF503658);
+  static const bgBlack = Color(0xFF151530);
+  static const maroon = Color(0xFF3C0A12);
+  static const skyBlue = Color(0xFF5884A9);
+  static const pink = Color(0xFFF774C4);
+  static const salmonOrange = Color(0xFFE5AB8A);
+  static const pinkishWhite = Color(0xFFdacae4);
+  static const textColor = Color(0xFF333333);
+}
